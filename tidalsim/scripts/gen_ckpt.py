5a8c3881dc5e543cdcafedc8d59c82bf59237078
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2023-11-06 15:37:01 -0800

    single checkpointing works again

--- tidalsim/scripts/gen_ckpt.py
+++ tidalsim/scripts/gen_ckpt.py
@@ -5,87 +5,53 @@ import stat
 import sys
 
 from tidalsim.util.cli import run_cmd, run_cmd_capture
-from tidalsim.spike_ckpt import arch_state_dump_cmds
+from tidalsim.util.spike_ckpt import *
 
 # This is a rewrite of the script here: https://github.com/ucb-bar/chipyard/blob/main/scripts/generate-ckpt.sh
 
-class CkptStrategy:
-    def spike_cmds_file(nharts: int) -> str:
-        pass
-
-# Take a single checkpoint after reaching `pc` and `n_insts` have committed from that point
-class SinglePCCkpt(CkptStrategy):
-    def __init__(self, pc: int, n_insts: int):
-        self.pc = pc
-        self.n_insts = n_insts
-
-    def spike_cmds_file(nharts: int) -> str:
-        # Run program until PC = self.pc
-        return f"until pc 0 {hex(self.pc)}\n" + \
-            # Run an additional n_insts instructions
-            f"rs {self.n_insts}\n" + \
-            # Dump arch state, with memory in mem.0x80000000.bin
-            arch_state_dump_cmds(f, nharts) + \
-            # Exit spike
-            "quit\n"
-
-# Take multiple checkpoints after reaching `pc` at every instruction commit point in `n_insts`
-# n_insts = [100, 1000, 2000] means
-# Take snapshots at the points where 100/1000/2000 instructions have committed
-class MultiPCCkpt(CkptStrategy):
-    def __init__(self, pc: int, n_insts: List[int]):
-        self.pc = pc
-        self.n_insts = n_insts
-
-    def spike_cmds_file(nharts: int) -> str:
-        n_insts
-        pass
-
 
 def gen_checkpoints(binary: Path, nharts: int, isa: str, strategy: CkptStrategy, dest_dir: Path) -> None:
-    dram_base = 0x8000_0000
-    dram_size = 0x1000_0000
-    basemem = f"{dram_base}:{dram_size}"
-    # TODO: add pmp CSR dumping commands to spike
-    spike_flags = f"-p{nharts} --pmpregions=0 --isa={isa} -m{basemem}"
-    basename = binary.name
-    checkpoint_dir = dest_dir / f"{basename}.{hex(strategy.pc)}.{strategy.n_insts}.loadarch"
-    if checkpoint_dir.exists():
-        shutil.rmtree(checkpoint_dir)
-    checkpoint_dir.mkdir(exist_ok=True)
+    base_dir = get_base_dir(dest_dir, binary)
+    print(f"Placing checkpoints in {base_dir}")
+    base_dir.mkdir(exist_ok=True)
+    assert isinstance(strategy, SinglePCCkpt)
+    ckpt_dir = get_ckpt_dir(dest_dir, binary, strategy.pc, strategy.n_insts)
+    print(f"Taking checkpoint in {ckpt_dir}")
+    if ckpt_dir.exists():
+        shutil.rmtree(ckpt_dir)
+    ckpt_dir.mkdir(exist_ok=True)
 
-    loadarch_file = checkpoint_dir / "loadarch"
-    rawmem_elf = checkpoint_dir / "raw.elf"
-    loadmem_elf = checkpoint_dir / "mem.elf"
-    cmds_file = checkpoint_dir / "cmds_tmp.txt"
-    spikecmd_file = checkpoint_dir / "spikecmd.sh"
+    # Commands for spike to run in debug mode
+    spike_cmds_file = base_dir / "spike_cmds.txt"
+    print(f"Generating state capture spike interactive commands in {spike_cmds_file}")
+    with spike_cmds_file.open('w') as f:
+        f.write(strategy.spike_cmds(nharts))
 
-    print(f"Generating state capture spike interactive commands in {cmds_file}")
-    with open(cmds_file, 'w') as f:
-        f.write(f"until pc 0 {hex(strategy.pc)}\n")
-        f.write(f"rs {strategy.n_insts}\n")
-        arch_state_dump(f, nharts)
-        f.write("quit\n")
+    # The spike invocation command itself
+    spike_cmd = get_spike_cmd(spike_cmds_file, isa, nharts, binary)
+    run_spike_cmd_file = base_dir / "run_spike.sh"
+    with run_spike_cmd_file.open('w') as f:
+        f.write(spike_cmd)
+    run_spike_cmd_file.chmod(run_spike_cmd_file.stat().st_mode | stat.S_IEXEC)
 
-    spikecmd = f"spike -d --debug-cmd={cmds_file.absolute()} {spike_flags} {binary.absolute()}"
-    with open(spikecmd_file, 'w') as f:
-        f.write(spikecmd)
-    spikecmd_file.chmod(spikecmd_file.stat().st_mode | stat.S_IEXEC)
 
-    print(f"Capturing state at checkpoint to {loadarch_file.absolute()}")
-    run_cmd(f"{spikecmd} 2> {loadarch_file.absolute()}", cwd=checkpoint_dir)
+    # Actually run spike
+    print(f"Running spike")
+    run_cmd(f"{spike_cmd} 2> {(ckpt_dir / 'loadarch').absolute()}", cwd=ckpt_dir)
 
     tohost = int(run_cmd_capture(f"riscv64-unknown-elf-nm {binary.absolute()} | grep tohost | head -c 16", Path.cwd()), 16)
     fromhost = int(run_cmd_capture(f"riscv64-unknown-elf-nm {binary.absolute()} | grep fromhost | head -c 16", Path.cwd()), 16)
-    print(f"Found tohost/fromhost in elf file, tohost: {hex(tohost)}, fromhost: {hex(fromhost)}")
+    print(f"Found tohost/fromhost in binary elf file, tohost: {hex(tohost)}, fromhost: {hex(fromhost)}")
 
     print(f"Compiling memory to elf")
-    spike_mem_out = checkpoint_dir / "mem.0x80000000.bin"
+    spike_mem_out = ckpt_dir / "mem.0x80000000.bin"
+    rawmem_elf = ckpt_dir / "raw.elf"
+    loadmem_elf = ckpt_dir / "mem.elf"
     # NOTE: objcopy and ld produce different elfs when given absolute paths vs relative paths! So I give them relative paths
     # here to maintain consistency with the original generate-ckpt.sh script
-    run_cmd(f"riscv64-unknown-elf-objcopy -I binary -O elf64-littleriscv {spike_mem_out.name} {rawmem_elf.name}", cwd=checkpoint_dir)
+    run_cmd(f"riscv64-unknown-elf-objcopy -I binary -O elf64-littleriscv {spike_mem_out.name} {rawmem_elf.name}", cwd=ckpt_dir)
     spike_mem_out.unlink()
-    run_cmd(f"riscv64-unknown-elf-ld -Tdata=0x80000000 -nmagic --defsym tohost={'0x%016x' % tohost} --defsym fromhost={'0x%016x' % fromhost} -o {loadmem_elf.name} {rawmem_elf.name}", cwd=checkpoint_dir)
+    run_cmd(f"riscv64-unknown-elf-ld -Tdata=0x80000000 -nmagic --defsym tohost={'0x%016x' % tohost} --defsym fromhost={'0x%016x' % fromhost} -o {loadmem_elf.name} {rawmem_elf.name}", cwd=ckpt_dir)
     rawmem_elf.unlink()
 
 def main():
--- tidalsim/util/spike_ckpt.py
+++ tidalsim/util/spike_ckpt.py
@@ -1,3 +1,59 @@
+from typing import List
+from pathlib import Path
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+@dataclass
+class CkptStrategy(ABC):
+    @abstractmethod
+    def spike_cmds(self, nharts: int) -> str:
+        return NotImplemented
+
+# Take a single checkpoint after reaching `pc` and `n_insts` have committed from that point
+@dataclass
+class SinglePCCkpt(CkptStrategy):
+    pc: int
+    n_insts: int
+
+    def spike_cmds(self, nharts: int) -> str:
+        # Run program until PC = self.pc
+        wait_for_pc = f"until pc 0 {hex(self.pc)}\n"
+        # Run an additional n_insts instructions
+        run_n_insts = f"rs {self.n_insts}\n"
+        # Dump arch state, with memory in mem.0x80000000.bin
+        dump_arch_state = arch_state_dump_cmds(nharts)
+        # Exit spike
+        exit_spike = "quit\n"
+        return wait_for_pc + run_n_insts + dump_arch_state + exit_spike
+
+# Take multiple checkpoints after reaching `pc` at every instruction commit point in `n_insts`
+# n_insts = [100, 1000, 2000] means
+# Take snapshots at the points where 100/1000/2000 instructions have committed
+@dataclass
+class MultiPCCkpt(CkptStrategy):
+    pc: int
+    n_insts: List[int]
+
+    def spike_cmds(self, nharts: int) -> str:
+        return NotImplemented
+
+def get_base_dir(dest_dir: Path, binary: Path) -> Path:
+    return dest_dir / f"{binary.name}.loadarch"
+
+def get_ckpt_dir(dest_dir: Path, binary: Path, pc: int, n_insts: int) -> Path:
+    return get_base_dir(dest_dir, binary) / f"{hex(pc)}.{n_insts}"
+
+def get_spike_flags(nharts: int, dram_size: int = 0x1000_0000, isa: str = 'rv64gc') -> str:
+    dram_base = 0x8000_0000
+    basemem = f"{dram_base}:{dram_size}"
+    # TODO: add pmp CSR dumping commands to spike
+    spike_flags = f"-p{nharts} --pmpregions=0 --isa={isa} -m{basemem}"
+    return spike_flags
+
+def get_spike_cmd(cmds_file: Path, isa: str, nharts: int, binary: Path) -> str:
+    spike_flags = get_spike_flags(nharts, isa=isa)
+    return f"spike -d --debug-cmd={cmds_file.absolute()} {spike_flags} {binary.absolute()}"
+
 def arch_state_dump_cmds(nharts: int) -> str:
     mem_dump = "dump\n"
     def reg_dump(h: int):
