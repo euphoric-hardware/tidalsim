3228e511c5b95aceb57822eedaf2a145c86ddf09
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2023-11-06 18:39:53 -0800

    multiple checkpoints seem to work

--- tidalsim/scripts/gen_ckpt.py
+++ tidalsim/scripts/gen_ckpt.py
@@ -42,23 +42,36 @@ def gen_checkpoints(strategy: CkptStrategy) -> None:
 
     # Actually run spike
     print(f"Running spike")
-    run_cmd(f"{spike_cmd} 2> {(base_dir / 'loadarch').absolute()}", cwd=base_dir)
-    sys.exit(0)
+    loadarch_file = base_dir / 'loadarch'
+    run_cmd(f"{spike_cmd} 2> {loadarch_file.absolute()}", cwd=base_dir)
 
-    tohost = int(run_cmd_capture(f"riscv64-unknown-elf-nm {binary.absolute()} | grep tohost | head -c 16", Path.cwd()), 16)
-    fromhost = int(run_cmd_capture(f"riscv64-unknown-elf-nm {binary.absolute()} | grep fromhost | head -c 16", Path.cwd()), 16)
+    # Spike emits a single loadarch file which needs to be split among the multiple checkpoints
+    loadarch_lines = loadarch_file.open('r').readlines()
+    split_idxs = [i for i, x in enumerate(loadarch_lines) if x == ':\n'] + [len(loadarch_lines)]
+    diffs = [(split_idxs[i] - split_idxs[i-1]) for i in range(1, len(split_idxs))]
+    assert all([x == diffs[0] for x in diffs]), "different number of lines per loadarch! something is wrong."
+    for i, ckpt_dir in enumerate(ckpt_dirs):
+        with (ckpt_dir / "loadarch").open('w') as f:
+            lines = loadarch_lines[split_idxs[i]:split_idxs[i+1]]
+            f.write(''.join(lines))
+    # loadarch_file.unlink()
+
+    # Capture tohost/fromhost memory addresses from original binary
+    tohost = int(run_cmd_capture(f"riscv64-unknown-elf-nm {strategy.binary.absolute()} | grep tohost | head -c 16", Path.cwd()), 16)
+    fromhost = int(run_cmd_capture(f"riscv64-unknown-elf-nm {strategy.binary.absolute()} | grep fromhost | head -c 16", Path.cwd()), 16)
     print(f"Found tohost/fromhost in binary elf file, tohost: {hex(tohost)}, fromhost: {hex(fromhost)}")
 
-    print(f"Compiling memory to elf")
-    spike_mem_out = ckpt_dir / "mem.0x80000000.bin"
-    rawmem_elf = ckpt_dir / "raw.elf"
-    loadmem_elf = ckpt_dir / "mem.elf"
-    # NOTE: objcopy and ld produce different elfs when given absolute paths vs relative paths! So I give them relative paths
-    # here to maintain consistency with the original generate-ckpt.sh script
-    run_cmd(f"riscv64-unknown-elf-objcopy -I binary -O elf64-littleriscv {spike_mem_out.name} {rawmem_elf.name}", cwd=ckpt_dir)
-    spike_mem_out.unlink()
-    run_cmd(f"riscv64-unknown-elf-ld -Tdata=0x80000000 -nmagic --defsym tohost={'0x%016x' % tohost} --defsym fromhost={'0x%016x' % fromhost} -o {loadmem_elf.name} {rawmem_elf.name}", cwd=ckpt_dir)
-    rawmem_elf.unlink()
+    for ckpt_dir in ckpt_dirs:
+        print(f"Compiling memory to elf in {ckpt_dir}")
+        spike_mem_out = ckpt_dir / "mem.0x80000000.bin"
+        rawmem_elf = ckpt_dir / "raw.elf"
+        loadmem_elf = ckpt_dir / "mem.elf"
+        # NOTE: objcopy and ld produce different elfs when given absolute paths vs relative paths! So I give them relative paths
+        # here to maintain consistency with the original generate-ckpt.sh script
+        run_cmd(f"riscv64-unknown-elf-objcopy -I binary -O elf64-littleriscv {spike_mem_out.name} {rawmem_elf.name}", cwd=ckpt_dir)
+        spike_mem_out.unlink()
+        run_cmd(f"riscv64-unknown-elf-ld -Tdata=0x80000000 -nmagic --defsym tohost={'0x%016x' % tohost} --defsym fromhost={'0x%016x' % fromhost} -o {loadmem_elf.name} {rawmem_elf.name}", cwd=ckpt_dir)
+        rawmem_elf.unlink()
 
 def main():
     # Parse string into an int with automatic radix detection
