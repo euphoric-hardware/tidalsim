44a1f628827c0b8c4574ea3171b876da0f04176e
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2023-11-11 17:15:02 -0800

    compute interval embeddings

--- tidalsim/scripts/tidalsim.py
+++ tidalsim/scripts/tidalsim.py
@@ -9,7 +9,7 @@ import logging
 
 from tidalsim.util.cli import run_cmd, run_cmd_capture, run_cmd_pipe
 from tidalsim.util.spike_ckpt import *
-from tidalsim.bb.spike import parse_spike_log, spike_trace_to_bbs, spike_trace_to_bbvs
+from tidalsim.bb.spike import parse_spike_log, spike_trace_to_bbs, spike_trace_to_bbvs, BasicBlocks
 
 # Runs directory structure
 # dest-dir
@@ -31,6 +31,7 @@ def main():
     # parser.add_argument('--isa', type=str, help='ISA to pass to spike [default rv64gc]', default='rv64gc')
     isa = "rv64gc" # hardcode this for now
     parser.add_argument('--dest-dir', type=str, required=True, help='Directory in which checkpoints are dumped')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
     args = parser.parse_args()
     binary = Path(args.binary).resolve()
     binary_name = binary.name
@@ -38,14 +39,16 @@ def main():
     dest_dir.mkdir(exist_ok=True)
     cwd = Path.cwd()
     assert args.interval_length > 1
+    if args.verbose:
+        logging.getLogger().setLevel(logging.DEBUG)
     logging.info(f"""Tidalsim called with:
     binary = {binary}
     interval_length = {args.interval_length}
     dest_dir = {dest_dir}""")
 
     # Create the binary directory if it doesn't exist
-    binary_hash = run_cmd_capture(f"sha256sum {binary.absolute()} | cut -d ' ' --fields 1", cwd=dest_dir)
     # Ignore the possibility of hash collisions as these can only happen for binaries that have the same name and the same first 8 hex characters of their hash
+    binary_hash = run_cmd_capture(f"sha256sum {binary.absolute()} | cut -d ' ' --fields 1", cwd=dest_dir)
     binary_dir = dest_dir / f"{binary_name}-{binary_hash[:8]}"
     binary_dir.mkdir(exist_ok=True)
     logging.info(f"Working directory set to {binary_dir}")
@@ -54,32 +57,50 @@ def main():
     spike_trace_file = binary_dir / "spike.trace"
     if spike_trace_file.exists():
         assert spike_trace_file.is_file()
+        logging.info(f"Spike trace file already exists in {spike_trace_file}, not rerunning spike")
     else:
+        logging.info(f"Spike trace doesn't exist at {spike_trace_file}, running spike")
         spike_cmd = get_spike_cmd(binary, n_harts, isa, debug_file=None, extra_args = "-l")
         run_cmd_pipe(spike_cmd, cwd=dest_dir, stderr=spike_trace_file)
 
     # Construct basic blocks from spike commit log if it doesn't already exist
+    bb: BasicBlocks
     spike_bb_file = binary_dir / "spike.bb"
     if not spike_bb_file.exists():
+        logging.info(f"Running spike commit log based BB extraction")
         with spike_trace_file.open('r') as f:
             spike_trace_log = parse_spike_log(f)
             bb = spike_trace_to_bbs(spike_trace_log)
             with spike_bb_file.open('wb') as bb_file:
                 pickle.dump(bb, bb_file)
-            print(bb)
+            logging.info(f"Spike commit log based BB extraction results saved to {spike_bb_file}")
+    else:
+        logging.info(f"Spike commit log based BB extraction already run, loading results from {spike_bb_file}")
+        with spike_bb_file.open('rb') as bb_file:
+            bb = pickle.load(bb_file)
+    logging.debug(f"Basic blocks: {bb}")
 
     # Given an interval length, compute the BBV-based interval embedding
     embedding_dir = binary_dir / str(args.interval_length)
     embedding_dir.mkdir(exist_ok=True)
     embedding_matrix = embedding_dir / "bbv.matrix"
+    matrix: np.ndarray
     if not embedding_matrix.exists():
-        with spike_bb_file.open('rb') as bb_file:
-            bb: BasicBlocks = pickle.load(bb_file)
+        logging.info(f"Computing BBV embedding in {embedding_matrix}")
         with spike_trace_file.open('r') as spike_trace:
             spike_trace_log = parse_spike_log(spike_trace)
             matrix = spike_trace_to_bbvs(spike_trace_log, bb, args.interval_length)
             with embedding_matrix.open('wb') as matrix_file:
                 pickle.dump(matrix, matrix_file)
-            print(matrix, matrix.shape)
+    else:
+        logging.info(f"BBV embedding already computed in {embedding_matrix}, loading")
+        with embedding_matrix.open('rb') as matrix_file:
+            matrix = pickle.load(matrix_file)
+    logging.debug(f"Embedding matrix:\n{matrix}")
+    logging.info(f"Embedding matrix shape: {matrix.shape}")
 
-    # 
+    # Perform clustering and select centroids
+    
+
+
+    # Capture arch checkpoints from spike
--- tidalsim/util/cli.py
+++ tidalsim/util/cli.py
@@ -5,20 +5,20 @@ from pathlib import Path
 import logging
 
 def run_cmd(cmd: str, cwd: Path) -> subprocess.CompletedProcess:
-    print(f"running {cmd}")
+    logging.info(f"Running \"{cmd}\"")
     result = subprocess.run(cmd, shell=True, stdout=sys.stdout, stderr=subprocess.STDOUT, cwd=cwd)
     assert result.returncode == 0, f"{cmd} failed with returncode {result.returncode}"
     return result
 
 def run_cmd_pipe(cmd: str, cwd: Path, stderr: Path) -> subprocess.CompletedProcess:
-    print(f"running \"{cmd}\" and redirecting stderr to {stderr}")
+    logging.info(f"Running \"{cmd}\" and redirecting stderr to {stderr}")
     with stderr.open('w') as stderr_file:
         result = subprocess.run(cmd, shell=True, stdout=sys.stdout, stderr=stderr_file, cwd=cwd)
         assert result.returncode == 0, f"{cmd} failed with returncode {result.returncode}"
         return result
 
 def run_cmd_capture(cmd: str, cwd: Path) -> str:
-    print(f"running {cmd}")
+    logging.info(f"Running \"{cmd}\" and capturing stdout")
     result = subprocess.run(cmd, shell=True, capture_output=True, cwd=cwd)
     stdout = result.stdout.decode('UTF-8').strip()
     assert result.returncode == 0, f"{cmd} failed with returncode {result.returncode} and stdout {stdout}"
