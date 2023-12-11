6582a8fd40f9374dc7ec43473d1be9586ced4c85
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2023-12-10 18:27:45 -0800

    ok looks like no more automation is possible for tag array injection

---
+++ pipecleaning/tag_array_injection/tag_array.v
@@ -0,0 +1,201 @@
+module tag_array_ext(
+  input  [5:0]  RW0_addr,
+  input         RW0_clk,
+  input  [87:0] RW0_wdata,
+  output [87:0] RW0_rdata,
+  input         RW0_en,
+  input         RW0_wmode,
+  input  [3:0]  RW0_wmask
+);
+  wire [5:0] mem_0_0_RW0_addr;
+  wire  mem_0_0_RW0_clk;
+  wire [21:0] mem_0_0_RW0_wdata;
+  wire [21:0] mem_0_0_RW0_rdata;
+  wire  mem_0_0_RW0_en;
+  wire  mem_0_0_RW0_wmode;
+  wire  mem_0_0_RW0_wmask;
+  wire [5:0] mem_0_1_RW0_addr;
+  wire  mem_0_1_RW0_clk;
+  wire [21:0] mem_0_1_RW0_wdata;
+  wire [21:0] mem_0_1_RW0_rdata;
+  wire  mem_0_1_RW0_en;
+  wire  mem_0_1_RW0_wmode;
+  wire  mem_0_1_RW0_wmask;
+  wire [5:0] mem_0_2_RW0_addr;
+  wire  mem_0_2_RW0_clk;
+  wire [21:0] mem_0_2_RW0_wdata;
+  wire [21:0] mem_0_2_RW0_rdata;
+  wire  mem_0_2_RW0_en;
+  wire  mem_0_2_RW0_wmode;
+  wire  mem_0_2_RW0_wmask;
+  wire [5:0] mem_0_3_RW0_addr;
+  wire  mem_0_3_RW0_clk;
+  wire [21:0] mem_0_3_RW0_wdata;
+  wire [21:0] mem_0_3_RW0_rdata;
+  wire  mem_0_3_RW0_en;
+  wire  mem_0_3_RW0_wmode;
+  wire  mem_0_3_RW0_wmask;
+  wire [21:0] RW0_rdata_0_0 = mem_0_0_RW0_rdata;
+  wire [21:0] RW0_rdata_0_1 = mem_0_1_RW0_rdata;
+  wire [21:0] RW0_rdata_0_2 = mem_0_2_RW0_rdata;
+  wire [21:0] RW0_rdata_0_3 = mem_0_3_RW0_rdata;
+  wire [65:0] _GEN_1 = {RW0_rdata_0_2,RW0_rdata_0_1,RW0_rdata_0_0};
+  split_tag_array_ext mem_0_0 (
+    .RW0_addr(mem_0_0_RW0_addr),
+    .RW0_clk(mem_0_0_RW0_clk),
+    .RW0_wdata(mem_0_0_RW0_wdata),
+    .RW0_rdata(mem_0_0_RW0_rdata),
+    .RW0_en(mem_0_0_RW0_en),
+    .RW0_wmode(mem_0_0_RW0_wmode),
+    .RW0_wmask(mem_0_0_RW0_wmask)
+  );
+  split_tag_array_ext mem_0_1 (
+    .RW0_addr(mem_0_1_RW0_addr),
+    .RW0_clk(mem_0_1_RW0_clk),
+    .RW0_wdata(mem_0_1_RW0_wdata),
+    .RW0_rdata(mem_0_1_RW0_rdata),
+    .RW0_en(mem_0_1_RW0_en),
+    .RW0_wmode(mem_0_1_RW0_wmode),
+    .RW0_wmask(mem_0_1_RW0_wmask)
+  );
+  split_tag_array_ext mem_0_2 (
+    .RW0_addr(mem_0_2_RW0_addr),
+    .RW0_clk(mem_0_2_RW0_clk),
+    .RW0_wdata(mem_0_2_RW0_wdata),
+    .RW0_rdata(mem_0_2_RW0_rdata),
+    .RW0_en(mem_0_2_RW0_en),
+    .RW0_wmode(mem_0_2_RW0_wmode),
+    .RW0_wmask(mem_0_2_RW0_wmask)
+  );
+  split_tag_array_ext mem_0_3 (
+    .RW0_addr(mem_0_3_RW0_addr),
+    .RW0_clk(mem_0_3_RW0_clk),
+    .RW0_wdata(mem_0_3_RW0_wdata),
+    .RW0_rdata(mem_0_3_RW0_rdata),
+    .RW0_en(mem_0_3_RW0_en),
+    .RW0_wmode(mem_0_3_RW0_wmode),
+    .RW0_wmask(mem_0_3_RW0_wmask)
+  );
+  assign RW0_rdata = {RW0_rdata_0_3,_GEN_1};
+  assign mem_0_0_RW0_addr = RW0_addr;
+  assign mem_0_0_RW0_clk = RW0_clk;
+  assign mem_0_0_RW0_wdata = RW0_wdata[21:0];
+  assign mem_0_0_RW0_en = RW0_en;
+  assign mem_0_0_RW0_wmode = RW0_wmode;
+  assign mem_0_0_RW0_wmask = RW0_wmask[0];
+  assign mem_0_1_RW0_addr = RW0_addr;
+  assign mem_0_1_RW0_clk = RW0_clk;
+  assign mem_0_1_RW0_wdata = RW0_wdata[43:22];
+  assign mem_0_1_RW0_en = RW0_en;
+  assign mem_0_1_RW0_wmode = RW0_wmode;
+  assign mem_0_1_RW0_wmask = RW0_wmask[1];
+  assign mem_0_2_RW0_addr = RW0_addr;
+  assign mem_0_2_RW0_clk = RW0_clk;
+  assign mem_0_2_RW0_wdata = RW0_wdata[65:44];
+  assign mem_0_2_RW0_en = RW0_en;
+  assign mem_0_2_RW0_wmode = RW0_wmode;
+  assign mem_0_2_RW0_wmask = RW0_wmask[2];
+  assign mem_0_3_RW0_addr = RW0_addr;
+  assign mem_0_3_RW0_clk = RW0_clk;
+  assign mem_0_3_RW0_wdata = RW0_wdata[87:66];
+  assign mem_0_3_RW0_en = RW0_en;
+  assign mem_0_3_RW0_wmode = RW0_wmode;
+  assign mem_0_3_RW0_wmask = RW0_wmask[3];
+endmodule
+
+module split_tag_array_ext(
+  input  [5:0]  RW0_addr,
+  input         RW0_clk,
+  input  [21:0] RW0_wdata,
+  output [21:0] RW0_rdata,
+  input         RW0_en,
+  input         RW0_wmode,
+  input         RW0_wmask
+);
+`ifdef RANDOMIZE_MEM_INIT
+  reg [31:0] _RAND_0;
+`endif // RANDOMIZE_MEM_INIT
+`ifdef RANDOMIZE_REG_INIT
+  reg [31:0] _RAND_1;
+  reg [31:0] _RAND_2;
+`endif // RANDOMIZE_REG_INIT
+  reg [21:0] ram [0:63];
+  wire  ram_RW_0_r_en;
+  wire [5:0] ram_RW_0_r_addr;
+  wire [21:0] ram_RW_0_r_data;
+  wire [21:0] ram_RW_0_w_data;
+  wire [5:0] ram_RW_0_w_addr;
+  wire  ram_RW_0_w_mask;
+  wire  ram_RW_0_w_en;
+  reg  ram_RW_0_r_en_pipe_0;
+  reg [5:0] ram_RW_0_r_addr_pipe_0;
+  assign ram_RW_0_r_en = ram_RW_0_r_en_pipe_0;
+  assign ram_RW_0_r_addr = ram_RW_0_r_addr_pipe_0;
+  assign ram_RW_0_r_data = ram[ram_RW_0_r_addr];
+  assign ram_RW_0_w_data = RW0_wdata;
+  assign ram_RW_0_w_addr = RW0_addr;
+  assign ram_RW_0_w_mask = RW0_wmask;
+  assign ram_RW_0_w_en = RW0_en & RW0_wmode;
+  assign RW0_rdata = ram_RW_0_r_data;
+  always @(posedge RW0_clk) begin
+    if (ram_RW_0_w_en & ram_RW_0_w_mask) begin
+      ram[ram_RW_0_w_addr] <= ram_RW_0_w_data;
+    end
+    ram_RW_0_r_en_pipe_0 <= RW0_en & ~RW0_wmode;
+    if (RW0_en & ~RW0_wmode) begin
+      ram_RW_0_r_addr_pipe_0 <= RW0_addr;
+    end
+  end
+// Register and memory initialization
+`ifdef RANDOMIZE_GARBAGE_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_INVALID_ASSIGN
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_REG_INIT
+`define RANDOMIZE
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+`define RANDOMIZE
+`endif
+`ifndef RANDOM
+`define RANDOM $random
+`endif
+`ifdef RANDOMIZE_MEM_INIT
+  integer initvar;
+`endif
+`ifndef SYNTHESIS
+`ifdef FIRRTL_BEFORE_INITIAL
+`FIRRTL_BEFORE_INITIAL
+`endif
+initial begin
+  `ifdef RANDOMIZE
+    `ifdef INIT_RANDOM
+      `INIT_RANDOM
+    `endif
+    `ifndef VERILATOR
+      `ifdef RANDOMIZE_DELAY
+        #`RANDOMIZE_DELAY begin end
+      `else
+        #0.002 begin end
+      `endif
+    `endif
+`ifdef RANDOMIZE_MEM_INIT
+  _RAND_0 = {1{`RANDOM}};
+  for (initvar = 0; initvar < 64; initvar = initvar+1)
+    ram[initvar] = _RAND_0[21:0];
+`endif // RANDOMIZE_MEM_INIT
+`ifdef RANDOMIZE_REG_INIT
+  _RAND_1 = {1{`RANDOM}};
+  ram_RW_0_r_en_pipe_0 = _RAND_1[0:0];
+  _RAND_2 = {1{`RANDOM}};
+  ram_RW_0_r_addr_pipe_0 = _RAND_2[5:0];
+`endif // RANDOMIZE_REG_INIT
+  `endif // RANDOMIZE
+end // initial
+`ifdef FIRRTL_AFTER_INITIAL
+`FIRRTL_AFTER_INITIAL
+`endif
+`endif // SYNTHESIS
+endmodule
