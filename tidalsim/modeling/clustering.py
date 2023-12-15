5ff9b8ac85ffd41ba9a774244580c29e19e8aad8
Charles Hong <charleshong@berkeley.edu>
2023-12-14 18:09:41 -0800

    add automatic num clusters selection

--- tidalsim/modeling/clustering.py
+++ tidalsim/modeling/clustering.py
@@ -1,7 +1,9 @@
 from typing import Tuple
+import logging
 
 import numpy as np
-from sklearn.metrics import pairwise_distances_argmin_min
+from sklearn.metrics import pairwise_distances_argmin_min, silhouette_score
+from sklearn.cluster import KMeans
 
 # Given a [centroid] vector (dim: n_features), a [matrix] (dim: n_samples X n_features),
 # a [labels] vector (dim: n_samples) that marks which cluster index a given sample is closest to, and
@@ -20,3 +22,27 @@ def get_closest_sample_to_centroid(centroid: np.ndarray, matrix: np.ndarray, lab
 def get_closest_samples_to_centroids(centroids: np.ndarray, samples: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     argmin, distances = pairwise_distances_argmin_min(X = centroids, Y = samples)
     return argmin, distances
+
+# Given a [matrix] (dim: n_samples X n_features), select the optimal number of K-means clusters
+# within a given range according to silhouette score.
+def pick_num_clusters(matrix: np.ndarray, max_clusters: int, random_state: int) -> int:
+    # make sure max_clusters isn't greater than the number of data points
+    max_clusters = min(max_clusters, matrix.shape[0])
+    range_n_clusters = range(2, max_clusters+1)
+    best_score: float = -1
+    best_n_clusters: int = 1
+    for n_clusters in range_n_clusters:
+        # Initialize the clusterer with n_clusters value
+        clusterer: KMeans = KMeans(n_clusters=n_clusters, n_init="auto", random_state=random_state)
+        cluster_labels: np.ndarray = clusterer.fit_predict(matrix)
+
+        # The silhouette_score gives the average value for all the samples.
+        # This gives a perspective into the density and separation of the formed
+        # clusters
+        silhouette_avg: float = silhouette_score(matrix, cluster_labels)
+        if silhouette_avg > best_score:
+            best_score = silhouette_avg
+            best_n_clusters = n_clusters
+        logging.debug(f"For n_clusters = {n_clusters}, the average silhouette_score is {silhouette_avg}")
+    logging.info(f"K-means best n_clusters {best_n_clusters} with silhouette score {round(best_score, 3)}")
+    return best_n_clusters
