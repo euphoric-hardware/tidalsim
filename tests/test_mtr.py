ae8fdb1fa21ef8cb1e248ca9c5505100c8882ee3
Vighnesh Iyer <vighnesh.iyer@gmail.com>
2024-02-13 13:53:41 -0800

    add 4 way cache reconstruction test

--- tests/test_mtr.py
+++ tests/test_mtr.py
@@ -4,7 +4,7 @@ from tidalsim.cache_model.cache import *
 from tidalsim.cache_model.mtr import *
 from tidalsim.util.spike_log import SpikeTraceEntry, SpikeCommitInfo, Op
 
-class TestMTR:
+class TestMTRCkpt:
     byte_offset_bits = 6
     block_size = 2**byte_offset_bits
     log = [
@@ -46,32 +46,57 @@ class TestMTR:
             })
         ]
 
-    def test_mtr_cache_reconstruction_1_way(self) -> None:
-        mtr = MTR(self.block_size, {
-            # Map of block address -> MTREntry
-            # Set 0
-            0: MTREntry(10, 3),
-            4: MTREntry(None, 5),
-            8: MTREntry(11, 5),
-            12: MTREntry(3, 9),
-            # Set 1
-            1: MTREntry(None, 4),
-            # Set 3
-            7: MTREntry(None, 8),
-        })
-        cache_params = CacheParams(32, self.block_size, n_sets=4, n_ways=1)
-        cache = mtr.as_cache(cache_params)
-        print(cache.array_pretty_str(Array.Tag))
-        # cache[way_idx][set_idx]
-        expected = {
-            (0, 0): ((8 >> cache_params.set_bits), CohStatus.Dirty),
-            (0, 1): ((1 >> cache_params.set_bits), CohStatus.Dirty),
-            (0, 2): (0, CohStatus.Nothing),
-            (0, 3): ((7 >> cache_params.set_bits), CohStatus.Dirty)
-        }
-        for ((way_idx, set_idx), (tag, coh)) in expected.items():
+class TestMTRCache:
+    byte_offset_bits = 6
+    block_size = 2**byte_offset_bits
+    mtr = MTR(block_size, {
+        # Map of block address -> MTREntry
+        # Set 0
+        0: MTREntry(10, 3),
+        4: MTREntry(None, 5),
+        8: MTREntry(11, 5),
+        12: MTREntry(3, 9),
+        16: MTREntry(12, None),
+        # Set 1
+        1: MTREntry(None, 4),
+        # Set 3
+        7: MTREntry(None, 8),
+        11: MTREntry(100, None)
+    })
+    def cache_params(self, ways: int) -> CacheParams:
+        return CacheParams(32, self.block_size, n_sets=4, n_ways=ways)
+
+    # Map from (way, set) -> (expected block_addr, expected coherency)
+    def check(self, expected: Dict[Tuple[int, int], Tuple[int, CohStatus]], cache: CacheState) -> None:
+        for ((way_idx, set_idx), (block_addr, coh)) in expected.items():
             block = cache.array[way_idx][set_idx]
-            assert block.tag == tag
+            assert block.tag == (block_addr >> cache.params.set_bits)
             assert block.coherency == coh
 
-    # TODO: test 2+ ways
+    def test_mtr_cache_reconstruction_1_way(self) -> None:
+        params = self.cache_params(1)
+        cache = self.mtr.as_cache(params)
+        print(cache.array_pretty_str(Array.Tag))
+        expected = {
+            (0, 0): (16, CohStatus.Dirty),
+            (0, 1): (1, CohStatus.Dirty),
+            (0, 2): (0, CohStatus.Nothing),
+            (0, 3): (11, CohStatus.Dirty)
+        }
+        self.check(expected, cache)
+
+    def test_mtr_cache_reconstruction_4_ways(self) -> None:
+        params = self.cache_params(4)
+        cache = self.mtr.as_cache(params)
+        print(cache.array_pretty_str(Array.Tag))
+        expected = {
+            (0, 0): (16, CohStatus.Dirty),
+            (1, 0): (8, CohStatus.Dirty),
+            (2, 0): (0, CohStatus.Dirty),
+            (3, 0): (12, CohStatus.Dirty),
+            (0, 1): (1, CohStatus.Dirty),
+            (0, 2): (0, CohStatus.Nothing),
+            (0, 3): (11, CohStatus.Dirty),
+            (1, 3): (7, CohStatus.Dirty)
+        }
+        self.check(expected, cache)
